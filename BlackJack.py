576f7cbcc66d2ee9c4cb9e00b76ca1a010983f62
どまおー <17107514+Dosugamea@users.noreply.github.com>
2018-03-07 00:06:57 +0900

    Made them class
    Maybe more useful.

--- BlackJack.py
+++ BlackJack.py
@@ -1,129 +1,93 @@
 import random
 
-#カードリストを表示させる
-def make_card_txt(cards,hide=False):
-    txts = ["","","","","","",""]
-    for card in cards:
-        txts[0] += "======= "
-        if (hide):
-            txts[1] += "|     | "
+class BlackJack():
+    num_dict = {
+        "11":"J",
+        "12":"Q",
+        "13":"K"
+    }
+    cards = [[m, n] for m in ['♣', '♦', '♥', '♠'] for n in range(1, 14)]
+
+    #カードを数える(10以上は10として数える)
+    def cnt_cards(self,P_or_D):
+        if P_or_D == "P":
+            cards = self.p_cards
         else:
-            txts[1] += "|"+mark_dict[card[0]]+"    | "
-        txts[2] += "|     | "
-        if (hide):
-            txts[3] += "|  ?  | "
-        else:
-            if len(str(card[1])) != 2:
-                txts[3] += "|  "+str(card[1])+"  | "
+            cards = self.d_cards
+        tt = 0
+        for card in cards:
+            if card[1] < 11:
+                tt += card[1]
             else:
-                if card[1] == 10:
-                    txts[3] += "| "+str(card[1])[0]+" "+str(card[1])[1]+" | "
-                else:
-                    txts[3] += "|  "+num_dict[str(card[1])]+"  | "
-        txts[4] += "|     | "
-        txts[5] += "|     | "
-        txts[6] += "======= "
-    for txt in txts:
-        print(txt)
+                tt += 10
+        return tt
+        
+    #カードを引く
+    def draw(self,cnt=2):
+        cards = []
+        for i in range(cnt):
+            c = random.choice(self.cards)
+            cards.append(c)
+            self.cards.remove(c)
+        return cards
+    
+    #判定 引き分けなら 0 負けなら -1 勝ちなら 1が返る
+    def decision(self):
+        pcnt = self.cnt_cards("P")
+        dcnt = self.cnt_cards("D")
+        if pcnt == dcnt: return 0
+        if dcnt > 21 or pcnt > dcnt: return 1
+        else: return -1
+    
+    #カードを追加する 21以下なら1 それ以外なら -1が返る
+    def hit(self):
+        self.p_cards.append(self.draw(1)[0])
+        if self.cnt_cards("P") < 22: return 1
+        else: return -1
+    
+    #一覧をそれぞれテキストで返す
+    def conv_txt(self):
+        toret = ["",""]
+        for card in self.p_cards:
+            if card[1] in self.num_dict: card[1] = self.num_dict[card[1]]
+            toret[0] += "%s%s "%(card[0],card[1])
+        for card in self.d_cards:
+            if card[1] in self.num_dict: card[1] = self.num_dict[card[1]]
+            toret[1] += "%s%s "%(card[0],card[1])
+        toret = [ret[:len(ret)-1] for ret in toret]
+        return toret
 
-#カードを数える(10以上は10として数える)
-def cnt_cards(cards):
-    tt = 0
-    for card in cards:
-        if card[1] < 11:
-            tt += card[1]
-        else:
-            tt += 10
-    return tt    
-
-#思ったんだけどこれ初めからマーク入れとけばよかった
-marks = ['club', 'diamond', 'heart', 'spade']
-mark_dict = {
-    "club":"♣",
-    "diamond":"♦",
-    "heart":"♥",
-    "spade":"♠"
-}
-num_dict = {
-    "11":"J",
-    "12":"Q",
-    "13":"K"
-}
-
-#トランプセットを作る
-numbers = range(1, 14)
-cards = [(m, n) for m in marks for n in numbers]
-#ディーラーに引かせる
-d_cards = []
-for i in range(2):
-    c = random.choice(cards)
-    d_cards.append(c)
-    cards.remove(c)
-while True:
-    #公式ルールに従い 17以上になるまで引く
-    if cnt_cards(d_cards) < 18:
-        c = random.choice(cards)
-        d_cards.append(c)
-        cards.remove(c)
-    else:
-        break
-#プレイヤーに引かせる
-p_cards = []
-for i in range(2):
-    c = random.choice(cards)
-    p_cards.append(c)
-    cards.remove(c)
-
-#ゲームループ    
-while True:
-    #メイン画面
-    print("ディーラー: Card x"+str(len(d_cards)))
-    make_card_txt(d_cards,True)
-    print("")
-    print("プレイヤー: Card x"+str(len(p_cards)))
-    print("トータル:"+str(cnt_cards(p_cards)))
-    make_card_txt(p_cards)
-    print("ヒットしますか? (1:Hit 2:Stand)")
-    while True:
-        inp = input(">>>")
-        if inp in ["1","2"]:
-            break
-    #もっかいぴょんぴょん
-    if inp == "1":
-        c = random.choice(cards)
-        p_cards.append(c)
-        cards.remove(c)
-        if cnt_cards(p_cards) > 21:
-            #プレイヤーバスト
-            print("\nショーダウン")
-            print("ディーラー: Card x"+str(len(d_cards)))
-            print("トータル:"+str(cnt_cards(d_cards)))
-            make_card_txt(d_cards)
-            print("")
-            print("プレイヤー: Card x"+str(len(p_cards)))
-            print("トータル:"+str(cnt_cards(p_cards)))
-            make_card_txt(p_cards)
-            print("プレイヤー: バスト")
-            print("\nプレイヤーの負け")
-            break
-    else:
-        #ショーダウン
-        print("\nショーダウン")
-        print("ディーラー: Card x"+str(len(d_cards)))
-        print("トータル:"+str(cnt_cards(d_cards)))
-        make_card_txt(d_cards)
-        print("")
-        print("プレイヤー: Card x"+str(len(p_cards)))
-        print("トータル:"+str(cnt_cards(p_cards)))
-        make_card_txt(p_cards)
-        if cnt_cards(d_cards) > 21:
-            print("ディーラー: バスト")
-            print("\nプレイヤーの勝ち")
-        else:
-            dtt = cnt_cards(d_cards)
-            ptt = cnt_cards(p_cards)
-            if dtt > ptt:
-                print("\nプレイヤーの負け")
+    #カードセットは クラス作った時点で決めとく()
+    def __init__(self):
+        #ディーラーに引かせる
+        self.d_cards = self.draw()
+        self.p_cards = self.draw()
+        #ディーラーは17以上になるまで引く
+        while True:
+            if self.cnt_cards("D") < 18:
+                self.d_cards.append(self.draw(1)[0])
             else:
-                print("\nプレイヤーの勝ち")
+                break
+
+#TEST-PLAY
+blackjacker = BlackJack()
+print("あなたの手札")
+print(blackjacker.conv_txt()[0])
+while True:
+    print("ヒットしますか? y/n")
+    yn = input('>>')
+    if yn == "y":
+        gohit = blackjacker.hit()
+        print("あなたの手札")
+        print(blackjacker.conv_txt()[0])
+        if gohit == -1: break
+    elif yn == "n":
         break
+print("\nショーダウン\n")
+datas = blackjacker.conv_txt()
+print("私 [%s] :"%(blackjacker.cnt_cards("D")))
+print(datas[1])
+print("あなた [%s] :"%(blackjacker.cnt_cards("P")))
+print(datas[0])
+print("")
+print(blackjacker.decision())
--- Poker.py
+++ Poker.py
@@ -1,34 +1,29 @@
 ﻿import random
-from time import sleep
 from collections import OrderedDict
 
-#トランプセットを作る
-numbers = range(1, 14)
-marks = ['club', 'diamond', 'heart', 'spade']
-cards = [[n, m] for m in marks for n in numbers]
-
-display_dict = {
-    "club":"♣",
-    "diamond":"♦",
-    "heart":"♥",
-    "spade":"♠",
-    "11":"J",
-    "12":"Q",
-    "13":"K"
-}
-
-#カード一覧表示
-def print_cards(cards):
-    text = ""
-    for l in cards:
-        if str(l[0]) in display_dict: 
-            text += "%s%s "%(display_dict[l[1]],display_dict[str(l[0])])
-        else:
-            text += "%s%s "%(display_dict[l[1]],l[0])
-    print(text)
-    
-#勝敗確認
-def check_win(d_res,p_res):
+class Poker():
+    cards = [[n, m] for m in ['club', 'diamond', 'heart', 'spade'] for n in range(1, 14)]
+    display_dict = {
+        "club":"♣",
+        "diamond":"♦",
+        "heart":"♥",
+        "spade":"♠",
+        "11":"J",
+        "12":"Q",
+        "13":"K"
+    }
+    jp_dict = {
+        "NoPair": "ノーペア",
+        "OnePair": "ワンペア",
+        "TwoPair": "ツーペア",
+        "ThreeCard": "スリーカード",
+        "Straight": "ストレート",
+        "Flash": "フラッシュ",
+        "FullHouse": "フルハウス",
+        "FourCard": "フォーカード",
+        "StraightFlash": "ストレートフラッシュ",
+        "RoyalStraightFlash": "ロイヤルストレートフラッシュ"
+    }
     id_dict = {
         "NoPair": 0,
         "OnePair": 1,
@@ -41,122 +36,146 @@ def check_win(d_res,p_res):
         "StraightFlash": 8,
         "RoyalStraightFlash": 9
     }
-    
-    d_res_id = list(d_res.keys())[0]
-    p_res_id = list(p_res.keys())[0]
-    
-    if id_dict[d_res_id] > id_dict[p_res_id]:
-        print("私の勝ちです!")
-    elif id_dict[d_res_id] < id_dict[p_res_id]:
-        print("あなたの勝ちです...")
-    else:
-        if d_res_id == -1 and p_res_id == -1:
-            print("ドローですね")
-        elif list(d_res.values())[0] > list(p_res.values())[0]:
-            print("私の勝ちです!")
-        elif list(d_res.values())[0] < list(p_res.values())[0]:
-            print("あなたの勝ちです...")
-        else:
-            print("ドローですね")
 
-#カード確認本体
-def check_cards(cards):
-    o_card = {}
-    flash = 0
-    straight = 1
-    o_card_type = cards[0][1]
-    o_card_num = o_card_ret = cards[0][0]
-    for n_card in cards:
-        #カード番号ごとの枚数辞書作成
-        if n_card[0] not in o_card:
-            o_card[n_card[0]] = 1
-        else:
-            o_card[n_card[0]] += 1
-        #フラッシュかどうかのcntを増やしていく
-        if o_card_type == n_card[1]:
-            flash += 1
-        #ストレートかどうかのcntを増やしていく
-        if o_card_num - 1 == n_card[0]:
-            o_card_num -= 1
-            straight += 1
-    #ストレート/ フラッシュなら ここで投げる(数字は?)
-    if flash == 5 and 10 in o_card and 11 in o_card and 12 in o_card and 13 in o_card and 1 in o_card: return {"RoyalStraightFlash":"-1"} 
-    if flash == 5 and straight == 5: return {"StraightFlash":"-1"}
-    if flash == 5: return {"Flash":"-1"}
-    if straight == 5: return {"Straight": o_card_ret}
-    to_ret = {}
-    #その他の確認
-    for card in o_card:
-        if o_card[card] == 4:
-            to_ret["FourCard"] = card
-        elif o_card[card] == 3:
-            to_ret["ThreeCard"] = card
-        elif o_card[card] == 2:
-            if "PairCard" not in to_ret:
-                to_ret["PairCard"] = card
+    #カード一覧表示
+    def print_cards(self,cards):
+        text = ""
+        for l in cards:
+            if str(l[0]) in self.display_dict: 
+                text += "%s%s "%(self.display_dict[l[1]],self.display_dict[str(l[0])])
             else:
-                to_ret["2ndPairCard"] = card
-    #フルハウス ツーペア ノーペア はここで整形
-    if "PairCard" in to_ret and "ThreeCard" in to_ret:
-        to_ret["FullHouse"] = to_ret["PairCard"] + to_ret["ThreeCard"]
-        del to_ret["PairCard"]
-        del to_ret["ThreeCard"]
-    if "PairCard" in to_ret and "2ndPairCard" in to_ret:
-        to_ret["TwoPair"] = to_ret["PairCard"] + to_ret["2ndPairCard"]
-        del to_ret["PairCard"]
-        del to_ret["2ndPairCard"]
-    if "PairCard" in to_ret:
-        to_ret["OnePair"] = to_ret["PairCard"]
-        del to_ret["PairCard"]
-    if to_ret == {}:
-        return {"NoPair":"-1"}
-    else:
-        return to_ret
+                text += "%s%s "%(self.display_dict[l[1]],l[0])
+        print(text)
+        
+    #カード結果表示
+    def show_down(self,P_or_D):
+        if P_or_D == "P":
+            cards = self.p_cards
+        else:
+            cards = self.d_cards
+        cards.sort(reverse=True)
+        self.print_cards(cards)
+        print(self.jp_dict[list(self.check_cards(cards).keys())[0]])
+        
+    #勝敗確認
+    def check_win(self):
+        d_res = self.check_cards(self.d_cards)
+        p_res = self.check_cards(self.p_cards)
+        d_res_id = list(d_res.keys())[0]
+        p_res_id = list(p_res.keys())[0]
+        
+        if self.id_dict[d_res_id] > self.id_dict[p_res_id]:
+            return -1
+        elif self.id_dict[d_res_id] < self.id_dict[p_res_id]:
+            return 1
+        else:
+            if d_res_id == -1 and p_res_id == -1:
+                return 0
+            elif list(d_res.values())[0] > list(p_res.values())[0]:
+                return -1
+            elif list(d_res.values())[0] < list(p_res.values())[0]:
+                return 1
+            else:
+                return 0
 
-#カードを引く
-def draw(cnt):
-    p_cards = []
-    #プレイヤーの手札を引く
-    for i in range(cnt):
-        c = random.choice(cards)
-        p_cards.append(c)
-        cards.remove(c)
-    p_cards.sort(reverse=True)
-    return p_cards
+    #カード確認本体
+    def check_cards(self,cards):
+        o_card = {}
+        flash = 0
+        straight = 1
+        o_card_type = cards[0][1]
+        o_card_num = o_card_ret = cards[0][0]
+        for n_card in cards:
+            #カード番号ごとの枚数辞書作成
+            if n_card[0] not in o_card:
+                o_card[n_card[0]] = 1
+            else:
+                o_card[n_card[0]] += 1
+            #フラッシュかどうかのcntを増やしていく
+            if o_card_type == n_card[1]:
+                flash += 1
+            #ストレートかどうかのcntを増やしていく
+            if o_card_num - 1 == n_card[0]:
+                o_card_num -= 1
+                straight += 1
+        #ストレート/ フラッシュなら ここで投げる(数字は?)
+        if flash == 5 and 10 in o_card and 11 in o_card and 12 in o_card and 13 in o_card and 1 in o_card: return {"RoyalStraightFlash":"-1"} 
+        if flash == 5 and straight == 5: return {"StraightFlash":"-1"}
+        if flash == 5: return {"Flash":"-1"}
+        if straight == 5: return {"Straight": o_card_ret}
+        to_ret = {}
+        #その他の確認
+        for card in o_card:
+            if o_card[card] == 4:
+                to_ret["FourCard"] = card
+            elif o_card[card] == 3:
+                to_ret["ThreeCard"] = card
+            elif o_card[card] == 2:
+                if "PairCard" not in to_ret:
+                    to_ret["PairCard"] = card
+                else:
+                    to_ret["2ndPairCard"] = card
+        #フルハウス ツーペア ノーペア はここで整形
+        if "PairCard" in to_ret and "ThreeCard" in to_ret:
+            to_ret["FullHouse"] = to_ret["PairCard"] + to_ret["ThreeCard"]
+            del to_ret["PairCard"]
+            del to_ret["ThreeCard"]
+        if "PairCard" in to_ret and "2ndPairCard" in to_ret:
+            to_ret["TwoPair"] = to_ret["PairCard"] + to_ret["2ndPairCard"]
+            del to_ret["PairCard"]
+            del to_ret["2ndPairCard"]
+        if "PairCard" in to_ret:
+            to_ret["OnePair"] = to_ret["PairCard"]
+            del to_ret["PairCard"]
+        if to_ret == {}:
+            return {"NoPair":"-1"}
+        else:
+            return to_ret
 
-print("私の手札を引きます...")
-d_cards = draw(5)
-print("プレイヤーの手札を引きます...")
-p_cards = draw(5)
+    #カードを引く
+    def draw(self,cnt):
+        cards = []
+        #プレイヤーの手札を引く
+        for i in range(cnt):
+            c = random.choice(self.cards)
+            cards.append(c)
+            self.cards.remove(c)
+        cards.sort(reverse=True)
+        return cards
+    
+    #カードを入れ替える
+    def change(self,id_text):
+        if id_text != "0":
+            o_pcards = self.p_cards
+            for i in id_text:
+                self.p_cards.remove(o_pcards[int(i)-1])
+                self.p_cards.insert(int(i)-1,self.draw(1)[0])
+        
+    #カードセットは クラス作った時点で決めとく()
+    def __init__(self):
+        #ディーラーに引かせる
+        self.d_cards = self.draw(5)
+        self.p_cards = self.draw(5)
+
+#TEST-PLAY
+Pokerer = Poker()
 print("\nあなたの手札")
-print_cards(p_cards)
+Pokerer.print_cards(Pokerer.p_cards)
 print("\n交換しますか?")
 print("(1,2,3,4,5 で指定して交換 交換しないなら0と入れてください)")
+#交換するカードの指定
 while True:
     tochg = input(">>")
     if len(tochg) < 6 and "0" not in tochg:
         break
     if tochg == "0":
         break
-    
-if tochg != "0":
-    o_pcards = p_cards
-    for i in tochg:
-        p_cards.remove(o_pcards[int(i)-1])
-        p_cards.insert(int(i)-1,draw(1)[0])
-print_cards(p_cards)
-
+Pokerer.change(tochg)
 print("\nショーダウン\n")
 print("私")
-d_cards.sort(reverse=True)
-d_res = check_cards(d_cards)
-print_cards(d_cards)
-print(list(d_res.keys())[0])
+Pokerer.show_down("D")
 print("")
-p_cards.sort(reverse=True)
-p_res = check_cards(p_cards)
 print("あなた")
-print_cards(p_cards)
-print(list(p_res.keys())[0])
+Pokerer.show_down("P")
 print("")
-check_win(d_res,p_res)
+print(Pokerer.check_win())
